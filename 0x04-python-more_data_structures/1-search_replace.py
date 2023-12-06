a627fe63f2ee9990141f50791d3c319ab3ad2f30
janaahmed77 <janaabdelmageed77@gmail.com>
2023-12-07 00:46:51 +0200

    jjj

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+# AUTHORS Selim
+
 def search_replace(my_list, search, replace):
-    return([replace if number == search else number for number in my_list])
+    return list(map(lambda x: replace if x == search else x, my_list))
